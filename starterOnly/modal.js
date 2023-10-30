85bccc2c669cebc3cf2a2355d2dc4b6094f26dc7
cpnsn <cpoinson@hotmail.fr>
2023-10-30 23:12:12 +0100

    changes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalSpan = document.querySelectorAll(".close");
+const modalCloseBtn = document.querySelector(".close-thanks");
+const thanks = document.querySelector(".thanks");
+const form = document.forms["reserve"]
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +24,80 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+modalSpan.forEach(span => {
+  span.addEventListener("click", function() {
+    closeModal();
+  });
+});
 
+modalCloseBtn.addEventListener("click", function() {
+  closeModal();
+});
+
+// close modal form and thank you message
+function closeModal() {
+  modalbg.style.display = "none";
+  thanks.style.display = "none";
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const inputs = form.querySelectorAll('.text-control');
+  let formIsValid = true;
+
+  inputs.forEach(input => {
+    const error = document.getElementById(`error-${input.id}`);
+    if (input.id === 'first' && !/^[A-Za-zÀ-ÖØ-öø-ÿ-' ]{2,}$/.test(input.value)) {
+      error.style.display = "block";
+      formIsValid = false; 
+    } else if (input.id === 'last' && !/^[A-Za-zÀ-ÖØ-öø-ÿ-' ]{2,}$/.test(input.value)) {
+      error.style.display = "block";
+      formIsValid = false;
+    } else if (input.id === 'email' && !/^\S+@\S+\.\S+$/.test(input.value)) {
+      error.style.display = "block";
+      formIsValid = false;
+    } else if (input.id === 'birthdate' && !input.value) {
+      error.style.display = "block";
+      formIsValid = false;
+    } else if (input.id === 'quantity' && (input.value.length < 1 || isNaN(input.value))) {
+      error.style.display = "block";
+      formIsValid = false;
+    } else {
+      error.style.display = "none";
+    }
+  });
+
+  const locationInputs = form.querySelectorAll('input[name="location"]');
+  let locationChecked = false;
+
+  locationInputs.forEach(input => {
+    if (input.checked) {
+      locationChecked = true;
+    }
+  });
+
+  const errorLocation = document.getElementById('error-location');
+  if (!locationChecked) {
+    errorLocation.style.display = "block";
+    formIsValid = false;
+  } else {
+    errorLocation.style.display = "none";
+  }
+
+  const errorCondition = document.getElementById('error-checkbox1');
+  const conditionInput = document.getElementById('checkbox1');
+  if (!conditionInput.checked) {
+    errorCondition.style.display = "block";
+    formIsValid = false;
+  } else {
+    errorCondition.style.display = "none";
+  }
+
+  if (formIsValid) {
+    // form.style.display = "none";
+    thanks.style.display = "flex";
+    modalbg.style.display = "none";
+  }
+});
